a351b9955a4c5ce2ec544835d73590b2451ca5c3
Raghav Mittal <raghav.mittal@slksoftware.com>
2025-11-10 18:14:29 +0530

    Add pattern notes and pratice problem

--- DSA/lecture 1/pattern/pattern2.py
+++ DSA/lecture 1/pattern/pattern2.py
@@ -24,7 +24,7 @@ def complex_pattern2(n:int=5):
 
         # star
         for j in range(2*n -(2*i +1)):
-            print("*", end="")
+            print('*', end="")
         
         for j in range(i):
             print(" ", end="")
@@ -33,6 +33,95 @@ def complex_pattern2(n:int=5):
         # space 
 
 complex_pattern2()
+print('-'*100)
+
+#---------------------------------------------------------------------
+def complex_pattern3(n:int=5):
+    complex_pattern1(n)
+    complex_pattern2(n)
+
+complex_pattern3()
+print('-'*100)      
+
+#---------------------------------------------------------------------
 
 
+def complex_pattern4(n: int = 5):
+    for i in range(1, 2 * n):  
+        value = i
+        if i > n:
+            value = 2 * n - i  # Decreasing part after midpoint
+        for j in range(value):  
+            print('*', end=' ')
+        print()
+    return None
+
+
+
+complex_pattern4()
+print('-'*100)   
+
+
+#---------------------------------------------------------------------
+
+
+def complex_pattern5(n:int =5):
+    for i in range(n):
+        if(i%2==0):
+            start =0
+        else:
+            start=1
+        for j in range(i+1):
+            print(start,end=" ")
+            start=1-start
+        print()
+    return None  
+
+complex_pattern5()
+print('-'*100)  
+
+
+#---------------------------------------------------------------------
+
+def complex_pattern6(n:int=5):
+    for i in range(n):
+        # numbers 
+        for j in range (i+1):
+            print(j+1,end="")
+
+        space=2*n-2*(i+1)
+        for j in range(space):
+            print(" ",end="")
+        
+        
+        for j in range(i + 1, 0, -1):
+            print(j,end="")
          
+        print()
+    return None 
+
+
+
+complex_pattern6()
+print('-'*100)  
+
+
+
+
+#---------------------------------------------------------------------
+
+def complex_pattern7(n:int=5):
+    start=1
+    for i in range(1,n+1,1):
+        for j in range(1,i+1,1):
+            print(start,end=" ")
+            start=start+1
+        print()
+    return None
+
+complex_pattern7()
+print('-'*100)  
+
+
+
+#---------------------------------------------------------------------
